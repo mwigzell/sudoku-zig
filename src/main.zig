14d828c1fe12518dcfcc5f30fab91b7b565bb692
Mark Wigzell <markwigzell@gmail.com>
2026-07-18 20:23:28 -0700

    feat(16.3): wire AnsiStyler in prod via main.zig

--- src/main.zig
+++ src/main.zig
@@ -3,13 +3,15 @@ const game_engine = @import("game_engine.zig");
 const ascii_renderer = @import("ascii_renderer.zig");
 const puzzle_gen = @import("puzzle_gen.zig");
 const logger = @import("logger.zig");
+const styler = @import("styler.zig");
 
 pub fn main(init: std.process.Init) anyerror!void {
     logger.Logger(.sudoku).debug("Starting sudoku game.", .{});
     var stdout_writer = std.Io.File.stdout().writer(init.io, &.{});
 
-    var r = ascii_renderer.AsciiRenderer.init(&stdout_writer.interface);
-    var engine = try game_engine.GameEngine(ascii_renderer.AsciiRenderer).init(puzzle_gen.PuzzleGen.easy(), &r);
-
+    var s = styler.AnsiStyler{};
+    const R = ascii_renderer.AsciiRenderer(styler.AnsiStyler);
+    var r = R.init(&stdout_writer.interface, &s);
+    var engine = try game_engine.GameEngine(R).init(puzzle_gen.PuzzleGen.easy(), &r);
     try engine.render();
 }
